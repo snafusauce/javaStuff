050104e3f161fb4a5254d0400d774e218382f065
Michael Saucedo <mikesauce@Michaels-MBP.attlocal.net>
2021-12-21 19:09:29 -0800

    p2

---
+++ Kingify.java
@@ -0,0 +1,11 @@
+public class Kingify {
+    
+    public static void main(String[] args){
+        if(args.length > 1){
+            System.out.println(args[0] + " Da King " + args[1]);
+        }
+        else{
+            System.out.println("You failed, run the program again and input exactly two names");
+        }
+    }
+}
